b2d9d09b061183c99b6f6418e0c16ffffa8a3f9f
Ubuntu <ubuntu@ip-172-31-36-18.ap-northeast-2.compute.internal>
2023-05-08 09:24:34 +0000

    [UPDATE] SSH 터널링 옵션 추가

--- config/superset_config.py
+++ config/superset_config.py
@@ -7,8 +7,11 @@ FEATURE_FLAGS = {
     'CLIENT_CACHE': True,
     'ENABLE_EXPLORE_JSON_CSRF_PROTECTION': True,
     'PRESTO_EXPAND_DATA': False,
-}
+    'SSH_TUNNELING': True,
+    }
 
+SSH_TUNNEL_MANAGER_CLASS = "superset.extensions.ssh.SSHManager"
+SSH_TUNNEL_LOCAL_BIND_ADDRESS = "127.0.0.1"
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # Generate a secure SECRET_KEY usng "openssl rand -base64 42"
